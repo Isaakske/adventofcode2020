83598225d9327bd515d007531d21f1ee43498a49
Isaakske <isaak.de.backer@hotmail.com>
2020-12-09 19:58:20 +0100

    day 8 part 2

--- 08/2.php
+++ 08/2.php
@@ -4,26 +4,48 @@ $inputString = require 'input.php';
 
 $inputArray = explode(PHP_EOL, $inputString);
 
-$acc = 0;
-$i = 0;
-$history = [];
-
-do {
-    $history[] = $i;
-
-    preg_match('/([a-z]{3}) ([-|+]\d+)/', $inputArray[$i], $instruction);
+$inputArrays = [];
+foreach ($inputArray as $x => $y) {
+    preg_match('/([a-z]{3}) ([-|+]\d+)/', $y, $instruction);
 
     $operation = $instruction[1];
-    $amount = (int) $instruction[2];
+    $amount = $instruction[2];
 
-    if ($operation === 'acc') {
-        $acc += $amount;
-        $i++;
-    } elseif ($operation === 'jmp') {
-        $i += $amount;
-    } elseif ($operation === 'nop') {
-        $i++;
+    if ($operation === 'acc') continue;
+
+    if ($operation === 'jmp') $operation = 'nop';
+    elseif ($operation === 'nop') $operation = 'jmp';
+
+    $copy = $inputArray;
+    $copy[$x] = $operation . ' ' . $amount;
+
+    $inputArrays[] = $copy;
+}
+
+foreach ($inputArrays as $input) {
+    $acc = 0;
+    $i = 0;
+    $history = [];
+
+    do {
+        $history[] = $i;
+
+        preg_match('/([a-z]{3}) ([-|+]\d+)/', $input[$i], $instruction);
+
+        $operation = $instruction[1];
+        $amount = (int) $instruction[2];
+
+        if ($operation === 'acc') {
+            $acc += $amount;
+            $i++;
+        } elseif ($operation === 'jmp') {
+            $i += $amount;
+        } elseif ($operation === 'nop') {
+            $i++;
+        }
+    } while (!in_array($i, $history) && array_key_exists($i, $input));
+
+    if ($i == count($inputArray)) {
+        echo $acc . PHP_EOL;
     }
-} while (!in_array($i, $history));
-
-echo $acc . PHP_EOL;
+}
