43a7650153df1744f93fa074694d1995c3821e77
Isaakske <isaak.de.backer@hotmail.com>
2020-12-09 19:16:34 +0100

    day 7

---
+++ 07/1.php
@@ -0,0 +1,38 @@
+<?php
+
+$inputString = require 'input.php';
+
+$inputArray = explode(PHP_EOL, $inputString);
+
+$needle = 'shiny gold bag';
+$bagMap = [];
+$bagOptions = [];
+
+foreach ($inputArray as $bag) {
+    preg_match_all('/\d?[a-z\s]* bag/', $bag, $bags);
+    $bags = $bags[0];
+
+    [$definition] = array_splice($bags, 0, 1);
+
+    $bagMap[$definition] = array_map(function ($b) {
+        return preg_replace('/\d+\s/', '', $b);
+    }, $bags);
+}
+
+function bagCanContainNeedle($needle, $bag, $bags, &$bagOptions, $bagMap) {
+    if (in_array($needle, $bags)) {
+        $bagOptions[] = $bag;
+    }
+
+    foreach ($bags as $b) {
+        if (array_key_exists($b, $bagMap)) {
+            bagCanContainNeedle($needle, $bag, $bagMap[$b], $bagOptions, $bagMap);
+        }
+    }
+}
+
+foreach ($bagMap as $bag => $bags) {
+    bagCanContainNeedle($needle, $bag, $bags, $bagOptions, $bagMap);
+}
+
+echo count(array_unique($bagOptions)) . PHP_EOL;
